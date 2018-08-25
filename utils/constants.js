6b40986363c53e91bacdaa53c3a1e50d64838897
kingcos <2821836721v@gmail.com>
2018-08-26 02:07:34 +0800

    Add new constants.js for save constants easily

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+// APIs
+const API_BASE_URL = "https://kingcos.top/"
+const API_ARTICLES_URL = API_BASE_URL.concat("perspective/issues")
+
+module.exports = {
+  API_BASE_URL: API_BASE_URL,
+  API_ARTICLES_URL: API_ARTICLES_URL
+}
