a6e59bd60d2aa4c421b32b28ea2db3b9ffca9581
kingcos <2821836721v@gmail.com>
2018-08-27 01:36:47 +0800

    Add new constants

--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,9 @@
 const API_BASE_URL = "https://kingcos.top/"
 const API_ARTICLES_URL = API_BASE_URL.concat("perspective/issues")
 
+// ERROR Messages
+const MESSAGE_ERROR_REQUEST_FAILED = "网络异常，请检查网络并重试"
+
 module.exports = {
   API_BASE_URL: API_BASE_URL,
   API_ARTICLES_URL: API_ARTICLES_URL
