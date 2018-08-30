18fcd69600b018b9622705a1ea3c454130d43d7d
kingcos <2821836721v@gmail.com>
2018-08-30 09:44:19 +0800

    Removed useless funcs & update refresh data funcs

--- pages/articles/articles.js
+++ pages/articles/articles.js
@@ -1,65 +1,73 @@
+var Network = require('../../utils/network.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    articles: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-  },
+    // Saved `this` Page
+    var that = this
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
+    wx.showLoading()
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
+    Network.fetchArticles(function (data) {
+      wx.hideLoading()
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
+      if (data.status == 0) {
+        that.setData({
+          articles: data.data
+        })
+      } else {
+        wx.showToast({
+          title: data.message,
+        })
+      }
+    })
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+    // Saved `this` Page
+    var that = this
+
+    wx.showLoading()
+
+    Network.fetchArticles(function (data) {
+      wx.hideLoading()
+
+      if (data.status == 0) {
+        that.setData({
+          articles: data.data
+        })
+      } else {
+        wx.showToast({
+          title: data.message,
+        })
+      }
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
