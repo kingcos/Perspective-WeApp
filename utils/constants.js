1e8c5338151e7f18d1b8802aa0bb52fa80d75295
kingcos <2821836721v@gmail.com>
2018-08-30 09:42:57 +0800

    Update API URL

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 // APIs
-const API_BASE_URL = "https://kingcos.top/"
-const API_ARTICLES_URL = API_BASE_URL.concat("perspective/issues")
+const API_BASE_URL = "https://kingcos.top/repos"
+const API_ARTICLES_URL = API_BASE_URL.concat("/kingcos/perspective/issues")
 
 // ERROR Messages
 const MESSAGE_ERROR_REQUEST_FAILED = "网络异常，请检查网络并重试"
