22489fb09cff1ec5adaf46a63d74f79058af7138
kingcos <2821836721v@gmail.com>
2018-08-31 09:43:28 +0800

    Fetch article details in page

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,65 +1,46 @@
-Page({
+var Network = require('../../utils/network.js')
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    
+    articleNumber: '',
+    article: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-  },
+    this.setData({
+      articleNumber: options.number
+    });
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
+    // Saved `this` Page
+    var that = this
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
+    wx.showLoading()
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
+    Network.fetchArticleDetails(options.number, function (data) {
+      wx.hideLoading()
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
+      if (data.status == 0) {
+        that.setData({
+          article: data.data
+        })
+      } else {
+        wx.showToast({
+          title: data.message,
+        })
+      }
+    })
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
     
   }
 })
